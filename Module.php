a7b20b21827ec355e7af74fc06d9716cafce3561
Andrew <andrew.panix@gmail.com>
2019-03-28 06:24:20 +0200

    111

--- Module.php
+++ Module.php
@@ -30,9 +30,7 @@ class Module extends WebModule
 
     public function getAdminSidebar()
     {
-        $mod = new \panix\engine\bootstrap\Nav;
-        $items = $mod->findMenu('shop');
-        return $items['items'];
+        return (new \panix\engine\bootstrap\BackendNav)->findMenu('shop')['items'];
     }
 
     public function getInfo()
